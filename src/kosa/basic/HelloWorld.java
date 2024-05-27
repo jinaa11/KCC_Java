bc7ad63fdfc0b4fd259e1d16c2df82cfecc291f6
jinaa11 <jinaa2047@gmail.com>
2024-05-27 17:41:00 +0900

    setting

---
+++ src/kosa/basic/HelloWorld.java
@@ -0,0 +1,17 @@
+package kosa.basic;
+
+public class HelloWorld {
+
+	public static void hello() {
+		System.out.println("¾È³ç~~");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello KCC~!~!");
+		System.out.println("Hello KOSA~!~!");
+
+		hello();
+	} // end main
+
+} // end class
