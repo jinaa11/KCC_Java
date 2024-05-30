f62eb6ff66b6f2043d438fa41501d9ae1cd65705
jinaa11 <jinaa2047@gmail.com>
2024-05-30 17:42:42 +0900

    PhoneInfo 수정, 삭제 기능 추가

--- src/kosa/phone/Main.java
+++ src/kosa/phone/Main.java
@@ -9,11 +9,10 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		Manager m = new Manager();
 
-		
 		// 만약 while문 안에서 Manager 객체를 생성하면 배열이 계속적으로 생겨서 어느 배열에 있는지 구분하기 어려움
 		// 3. 검색 추가 - 이름
 		while (true) {
-			System.out.println("1.추가 2.전체출력 3.검색 4.종료");
+			System.out.println("1.추가 2.전체출력 3.검색 4.수정 5.삭제 6.종료");
 			System.out.print("메뉴 입력: ");
 			String menu = sc.nextLine();
 
@@ -28,6 +27,12 @@ public class Main {
 				m.searchPhoneInfo();
 				break;
 			case "4":
+				m.updatePhoneInfo();
+				break;
+			case "5":
+				m.deletePhoneInfo();
+				break;
+			case "6":
 				System.out.println("프로그램 종료");
 				return;
 			}
--- src/kosa/phone/Manager.java
+++ src/kosa/phone/Manager.java
@@ -8,12 +8,12 @@ public class Manager {
 	private PhoneInfo arr[];
 	private Scanner sc;
 	private int count;
-	
+
 	public Manager() {
 		arr = new PhoneInfo[10];
 		sc = new Scanner(System.in);
 	}
-	
+
 	public void addPhoneInfo() {
 		// 키보드로부터 이름, 전화번호, 생년월일을 입력 받아
 		// PhoneInfo 객체를 생성해서 배열에 추가하는 것
@@ -23,34 +23,65 @@ public class Manager {
 		String phoneNo = sc.nextLine();
 		System.out.print("생년월일: ");
 		String birth = sc.nextLine();
-		
+
 		arr[count++] = new PhoneInfo(name, phoneNo, birth);
 	};
-	
+
 	public void listPhoneInfo() {
-		for(int i = 0; i < count; i++) {
+		for (int i = 0; i < count; i++) {
 			arr[i].show();
 		}
 	}
-	
+
 	public void searchPhoneInfo() {
 		// 검색하고자 하는 이름으로 1개의 PhoneInfo 객체의 내용 출력
 		System.out.print("검색할 이름: ");
 		String name = sc.nextLine();
 		int idx = -1;
-		
-		for(int i = 0; i < count; i++) {
-			if(name.equals(arr[i].getName())) {
+
+		for (int i = 0; i < count; i++) {
+			if (name.equals(arr[i].getName())) {
 				System.out.println("검색한 이름: " + name);
 				arr[i].show();
 				idx = i;
 			}
 		}
-		
+
 		// 검색된 내용이 없는 경우
-		if(idx == -1) {
+		if (idx == -1) {
 			System.out.println("존재하지 않는 이름입니다.");
 		}
+
+	}
+
+	public void updatePhoneInfo() {
+		// 이름을 입력 => 해당 phoneInfo 추출 => 수정 전화번호 입력 => 전화번호 수정이 완료
+		System.out.print("수정할 이름 입력: ");
+		String name = sc.nextLine();
+		String newPhoneNo = null;
 		
+		for(int i = 0; i < count; i++) {
+			if(name.equals(arr[i].getName())) {
+				System.out.print("수정할 전화번호 입력: ");
+				newPhoneNo = sc.nextLine();
+				arr[i].setPhoneNo(newPhoneNo);
+			}
+		}
+	}
+
+	public void deletePhoneInfo() {
+		// 이름을 입력 => 대상 객체 위치 검색 => 인덱스 찾기 => 해당 객체 삭제 (뒤에 부터 앞으로 1칸씩 옮기고 마지막 것을 삭제 - 인덱스 하나 감소)
+		System.out.print("삭제할 이름 입력: ");
+		String name = sc.nextLine();
+		
+		for(int i = 0; i < count; i++) {
+			if(name.equals(arr[i].getName())) {
+				for(int j = i; j < count - 1; j++) {
+					arr[j] = arr[j+1];
+				}
+				count--;
+			}
+		}
+		System.out.println("삭제 완료");
 	}
 }
